9404898f055c0b82dc82ab1acdb95cf069f5d80e
BigHeadliner <bigheadliner@gmail.com>
2022-11-06 01:23:50 +0200

    best product section is ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function () { 
-     
+    
+    $("#product-1").rateYo({
+        rating: 3, 
+        starWidth: "23px"
+      }); 
+       
+      $("#product-2").rateYo({
+        rating: 4, 
+        starWidth: "23px"
+      }); 
+       
+          
+      $("#product-3").rateYo({
+        rating: 5, 
+        starWidth: "23px"
+      });
    
 
 });
