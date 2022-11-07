632e277fec9543051f1757f54c0cfbf8f12998cf
BigHeadliner <bigheadliner@gmail.com>
2022-11-07 02:42:09 +0200

    review section done

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,11 @@
-$(function () { 
+$(function () {  
+   
+    $('.review__slider').slick({ 
+      prevArrow:'<button type="button" class="slick-prev slick-arrow"><img src="images/review/arrow-prev.svg" alt=""></button>', 
+      nextArrow:'<button type="button" class="slick-next slick-arrow"><img src="images/review/arrow-next.svg" alt=""></button>',  
+      fade: true, 
+      dots: true,
+    });
     
     $("#product-1").rateYo({
         rating: 3, 
