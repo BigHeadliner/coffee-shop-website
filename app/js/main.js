1f40c3880d53a70d74f6b027f0769eabce5eb2dc
BigHeadliner <bigheadliner@gmail.com>
2022-11-16 23:28:50 +0200

    blog section ready

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,8 @@ $(function () {
       prevArrow:'<button type="button" class="slick-prev slick-arrow"><img src="images/review/arrow-prev.svg" alt=""></button>', 
       nextArrow:'<button type="button" class="slick-next slick-arrow"><img src="images/review/arrow-next.svg" alt=""></button>',  
       fade: true, 
-      dots: true,
+      dots: true, 
+      autoplay: true,
     });
     
     $("#product-1").rateYo({
